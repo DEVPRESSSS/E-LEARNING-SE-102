d93260c16ffa89563057778aa28d919dbf0792b4
devpresss xmont <xmontemorjerald@gmail.com>
2026-01-10 09:15:21 +0800

    Publish

---
+++ publish/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿
+
+// HOME/INDEX MENU ACTIVE CSS
+document.querySelectorAll('.course-navigation').forEach(el => {
+    el.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent page reload
+
+        // Remove 'active' from all links
+        document.querySelectorAll('.course-menu a')
+            .forEach(a => a.classList.remove('active'));
+
+        // Add 'active' to the clicked link
+        this.classList.add('active');
+
+        // Navigate to the Razor-generated URL
+        window.location.href = this.getAttribute('href');
+    });
+});
+
