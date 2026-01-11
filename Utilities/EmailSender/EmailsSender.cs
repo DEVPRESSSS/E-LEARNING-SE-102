96fe53596115c3aef9126ea552a5448ab898222c
devpresss xmont <xmontemorjerald@gmail.com>
2026-01-11 23:22:27 +0800

    Added sending of email in EmailSender

--- Utilities/EmailSender/EmailsSender.cs
+++ Utilities/EmailSender/EmailsSender.cs
@@ -1,15 +1,48 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace E_LEARNING_SE_102_PROJECT.Utilities.EmailSender
 {
     public class EmailsSender : IEmailSender
     {
-      
-
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public readonly string _sendGrid;
+        public EmailsSender(IConfiguration configuration)
         {
-            return Task.CompletedTask;
+            _sendGrid = Environment.GetEnvironmentVariable("SEND_GRID_API_KEY") ?? string.Empty;    
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            try
+            {
+                //Pass the sendgrid secret to SendGridClient
+                var client = new SendGridClient(_sendGrid);
+
+                //Declare the sender
+                var from = new EmailAddress("montemorjeraldd@gmail.com", "Password recovery!");
+
+                //Get the receiver and the html message
+                var to = new EmailAddress(email);
+
+                //Create a singel email sender
+                var message = MailHelper.CreateSingleEmail(from, to, subject,"", htmlMessage);
+
+                //Send the email
+                var response = await client.SendEmailAsync(message);
+
+                //Check if the status code is ok if not it will throw error
+                if (response.StatusCode != System.Net.HttpStatusCode.Accepted)
+                {
+                    var errorBody = await response.Body.ReadAsStringAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
