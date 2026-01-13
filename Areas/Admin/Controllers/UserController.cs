275cddd74a373c0b3d61c931678c6c9bbb47f3fb
montemorjeraldd@gmail.com <DEVPRESSSS>
2026-01-13 18:07:23 +0800

    Fixed bug in Upsert and added simple UI

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -17,15 +17,53 @@ namespace E_LEARNING_SE_102_PROJECT.Areas.Admin.Controllers
         public IActionResult Index()
         {
 
-            var users = _context.Users.ToList();
+            var users = _context.AppUser.ToList();
             return View(users);
         }
 
-        public IActionResult Upsert(ApplicationUser user)
+        public IActionResult Upsert(string? id) 
         {
-                    
+            if (id == null)
+                return View();
 
-            return View(user);
+            var obj = _context.AppUser
+                .FirstOrDefault(x=>x.Id == id);
+            if (obj == null)
+                return NotFound();
+        
+            return View(obj);
+        }
+        [HttpPost]
+        [ActionName("Upsert")]
+        public IActionResult CreateAndUpdate(ApplicationUser user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            if(string.IsNullOrEmpty(user.Id))
+            {
+                user.EmailConfirmed = true;
+                _context.AppUser.Add(user);
+            }
+            else
+            {
+                var obj = _context.AppUser.FirstOrDefault(x => x.Id == user.Id);
+                if (obj == null) return NotFound();
+
+                obj.FirstName = user.FirstName;
+                obj.MiddleName = user.MiddleName;
+                obj.LastName = user.LastName;
+                obj.Email = user.Email;
+                obj.PasswordHash = user.PasswordHash;
+
+                _context.AppUser.Update(obj);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
