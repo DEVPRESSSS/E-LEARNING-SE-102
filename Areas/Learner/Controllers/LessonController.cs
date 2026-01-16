c6451dafaac0452f90735a2f551d7e8f3421268f
devpresss xmont <xmontemorjerald@gmail.com>
2026-01-16 12:28:11 +0800

    Try hosting downgrade to 6

--- Areas/Learner/Controllers/LessonController.cs
+++ Areas/Learner/Controllers/LessonController.cs
@@ -1,14 +1,34 @@
-﻿using E_LEARNING_SE_102_PROJECT.Utilities;
+﻿using E_LEARNING_SE_102_PROJECT.Data;
+using E_LEARNING_SE_102_PROJECT.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_LEARNING_SE_102_PROJECT.Areas.Learner.Controllers
 {
     [Area(SD.Learner)]
     public class LessonController : Controller
     {
-        public IActionResult ViewLesson()
+        private readonly ApplicationDbContext _context;
+        public LessonController( ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public IActionResult ViewLesson(string id)
+        {
+            if(id == null)
+            {
+                ModelState.AddModelError("", "No content id found");
+                return View();
+            }
+
+            var obj = _context.Contents
+                     .Include(x=>x.Lesson)
+                    .FirstOrDefault(x=>x.ContentId == id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
     }
 }
